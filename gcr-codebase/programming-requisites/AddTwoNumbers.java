03a2357766211eac89b1e108c01ed1759a476b52
Khushi-Pathak-25 <khushipathak.2503@gmail.com>
2025-12-16 17:24:11 +0530

    [Khushi] : added a program print sum of two numbers

---
+++ gcr-codebase/programming-requisites/AddTwoNumbers.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+class AddTwoNumbers{
+   public static void main(String[]args){
+    Scanner s=new Scanner(System.in);
+     System.out.println("Enter to numbers: ");
+     int a=s.nextInt();
+     int b=s.nextInt();
+     System.out.println("Sum: "+ (a+b));
+}
+ }
