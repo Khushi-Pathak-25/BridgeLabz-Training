282ce72199f189f11a021e0e6d49acd05a97f9bf
Khushi-Pathak-25 <khushipathak.2503@gmail.com>
2025-12-17 23:50:02 +0530

    [Khushi] : Add program to calculate average PCM percentage

---
+++ gcr-codebase/programming-elements/Level 1/CalculateSamAverageMarks.java
@@ -0,0 +1,13 @@
+class CalculateSamAverageMarks{
+    public static void main(String[]args){
+    //Sam’s mark in Maths, Physics and Chemistry 
+    int marksInMaths= 94, marksInPhysics=95, marksInChemistry=96;
+
+    //Calculate the average percent mark of Sam
+    double averagePercentMark=(marksInMaths+marksInPhysics+marksInChemistry)/3;
+
+    //Print the average percent mark of Sam
+    System.out.println("Sam's average mark in PCM is "+ averagePercentMark );
+
+    }
+}
