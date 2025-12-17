2ea52962a9547bb6a695613a94c11ace0e198a93
Khushi-Pathak-25 <khushipathak.2503@gmail.com>
2025-12-17 23:33:23 +0530

    [Khushi] : added program to find the age of Harry

---
+++ gcr-codebase/programming-elements/Level 1/AgeOfHarry.java
@@ -0,0 +1,12 @@
+class AgeOfHarry{
+    public static void main(String[]args){
+
+    //Harry's birth year
+    int birthYear=2000;
+    // Current year value
+    int currentYear=2024;
+
+    System.out.println("Harry's age in 2024 is "+(currentYear-birthYear));
+
+    }
+}
