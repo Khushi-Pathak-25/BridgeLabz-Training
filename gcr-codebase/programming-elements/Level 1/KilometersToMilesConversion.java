669d4c170ae6f9b5769c12e48949e037f011a997
Khushi-Pathak-25 <khushipathak.2503@gmail.com>
2025-12-17 23:55:20 +0530

    [Khushi] : Add program of distance conversion from kilometers to miles

---
+++ gcr-codebase/programming-elements/Level 1/KilometersToMilesConversion.java
@@ -0,0 +1,12 @@
+class KilometersToMilesConversion{
+    public static void main(String args[]){
+    
+    //distance in kilometers
+    double distanceInKm= 10.8;
+    
+    // 1 km = 1.6 miles
+    final double KM_TO_MILE= 1.6;
+    
+    System.out.println("The distance "+distanceInKm+" km in miles is "+(distanceInKm * KM_TO_MILE));
+    }
+}
