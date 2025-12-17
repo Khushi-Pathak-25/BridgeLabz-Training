eb79abf9ae30c57bdce52cad8f9ce5c9950e47c1
Khushi-Pathak-25 <khushipathak.2503@gmail.com>
2025-12-18 00:07:04 +0530

    [Khushi] : Add profit and loss calculation with percentage

---
+++ gcr-codebase/programming-elements/Level 1/CalculateProfitAndLoss.java
@@ -0,0 +1,15 @@
+class CalculateProfitAndLoss{
+    public static void main(String args[]){
+    //cost price and selling price in INR
+    double costPrice=129, sellingPrice=191;
+
+    //Calculate Profit
+    double profit = sellingPrice - costPrice;
+    //Calculate Profit Percentage
+    double profitPercentage = profit / costPrice * 100;
+    
+    System.out.println("The Cost Price is INR "+costPrice+" and Selling Price is INR "+sellingPrice +'\n'+"The 
+    Profit is INR "+profit+" and the Profit Percentage is "+ profitPercentage);
+
+    }
+}
