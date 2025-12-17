5db0eff6aa69c8133060080e831ddd5c3c43249e
Khushi-Pathak-25 <khushipathak.2503@gmail.com>
2025-12-18 00:11:10 +0530

    [Khushi] : Add program to divide pens equally and find remainder

---
+++ gcr-codebase/programming-elements/Level 1/DistributionOfPen.java
@@ -0,0 +1,10 @@
+class DistributionOfPen{
+    public static void main(String[]args){
+    //Total Pen
+    int totalPen=14;
+    //Total Students
+    int totalStudents=3;
+
+    System.out.println("The Pen Per Student is "+(totalPen/totalStudents)+" and the remaining pen not distributed is "+(totalPen%totalStudents));
+    }
+}
