afe29b889959bf1451ccedfdddeeff097c9a084b
Khushi-Pathak-25 <khushipathak.2503@gmail.com>
2025-12-18 15:29:21 +0530

    [Khushi] : Add a program to check whether a number is positive, negative, or zero

---
+++ gcr-codebase/control-flows/Level 1/NumberPositiveNegativeOrZero.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+class NumberPositiveNegativeOrZero{
+
+    public static void main(String[]args){
+ 
+        //Creating Scanner class object
+	Scanner input = new Scanner(System.in);
+
+        //Taking input for number
+        System.out.print("\nEnter number : ");
+        int number = input.nextInt();
+       
+        //Checking whether a number is positive, negative, or zero
+        if(number>0){
+            System.out.println("positive");
+        }
+        else if(number<0){
+            System.out.println("negative");
+        }
+        else{
+            System.out.println("zero");
+        }
+   }
+}
