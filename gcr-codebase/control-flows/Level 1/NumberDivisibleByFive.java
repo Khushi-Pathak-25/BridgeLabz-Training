9c7ac2c1d8639c4735f5cb098eaf53d6c0916968
Khushi-Pathak-25 <khushipathak.2503@gmail.com>
2025-12-18 12:43:15 +0530

    [Khushi] : Add program to check if a number is divisible by 5

---
+++ gcr-codebase/control-flows/Level 1/NumberDivisibleByFive.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+class NumberDivisibleByFive{
+    public static void main(String args[]){
+    //Creating Scanner Class object
+    Scanner input=new Scanner(System.in);
+
+    //Taking number from user
+    System.out.print("\nEnter a number : ");
+    int number=input.nextInt();
+
+    //Checking whether a number is divisible by 5
+    System.out.println("Is the number "+ number +" divisible by 5? "+(number%5==0));
+    }
+}
