b1422e60f0d8487cf3fb5d0b92d82dd94bd81055
Khushi-Pathak-25 <khushipathak.2503@gmail.com>
2025-12-20 02:37:46 +0530

    [Khushi] : Add a program factors of a number using while loop

---
+++ gcr-codebase/control-flows/Level 2/FactorsOfNumber2.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class FactorsOfNumber2 {
+
+    public static void main(String[] args) {
+
+        // Creating Scanner object
+        Scanner input = new Scanner(System.in);
+
+        // Taking input
+        System.out.print("Enter a number: ");
+        int number = input.nextInt();
+
+        // Check if the number is a positive 
+        if (number <= 0) {
+            System.out.println("Please enter a positive integer.");
+        }
+        else {
+
+             // Initialize number 
+             int i = 1;
+
+             // Run while loop
+             while (i <= number) {
+
+                // Check if number is divisible by i
+                if (number % i == 0) {
+                    System.out.println(i);
+                }
+
+                // Increment i
+                i++;
+            }
+        }        
+
+        input.close();
+    }
+}
