05c057b16d0d4fe03fa5689d47dabe5cee275800
Khushi-Pathak-25 <khushipathak.2503@gmail.com>
2025-12-19 22:44:49 +0530

    [Khushi] : Add a program for Copy 2D array elements into the 1D array

---
+++ gcr-codebase/arrays/Level 1/2DArrayTo1DArray.java
@@ -0,0 +1,58 @@
+import java.util.Scanner;
+
+public class 2DArrayTo1DArray {
+
+    public static void main(String[] args) {
+
+        //Creating Scanner input
+        Scanner input = new Scanner(System.in);
+
+        //Enter number of rows
+        System.out.print("Enter number of rows: ");
+        int rows = input.nextInt();
+
+        //Enter number of columns
+        System.out.print("Enter number of columns: ");
+        int columns = input.nextInt();
+
+        //Creating a 2D array
+        int[][] matrix = new int[rows][columns];
+
+        // Taking input values for the 2D array
+        System.out.println("Enter elements of the 2D array:");
+        for (int i = 0; i < rows; i++) { 
+            for (int j = 0; j < columns; j++) { 
+                matrix[i][j] = sc.nextInt();
+            }
+        }
+        
+        //Creating 1D array of size rows * columns
+        int[] array = new int[rows * columns];
+
+        //Creating index variable for storing elements
+        int index = 0;
+
+        //Copying elements from 2D array to 1D array
+        for (int i = 0; i < rows; i++) {       
+            for (int j = 0; j < columns; j++) { 
+                array[index] = matrix[i][j];  
+                index++;                         
+            }
+        }
+
+        //Displaying the 2D array
+        System.out.println("\n2D Array:");
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+        //Displaying the copied 1D array
+        System.out.println("\n1D Array:");
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + " ");
+        }
+    }
+}
