c7076d9133824c314445ababb4e762cd0f1ee9b3
Khushi-Pathak-25 <khushipathak.2503@gmail.com>
2025-12-20 02:28:35 +0530

    [Khushi] : Add a program factors of a number

---
+++ gcr-codebase/control-flows/Level 2/FactorsOfNumber.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class FactorsOfNumber {
+
+    public static void main(String[] args) {
+
+        // Creating Scanner object
+        Scanner input = new Scanner(System.in);
+
+        // Taking input
+        System.out.print("Enter a number: ");
+        int number = input.nextInt();
+
+        // Check if the number is a positive 
+        if (number <= 0) {
+            System.out.println("Please enter a positive integer.");
+        }
+        else {
+            System.out.println("Factors of " + number + " are:");
+         
+            for (int i = 1; i <= number; i++) {
+
+                // Check if number is divisible by i
+                if (number % i == 0) {
+                    System.out.println(i);
+                }
+            }
+        }
+
+        input.close();
+    }
+}
