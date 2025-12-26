be1d4fae6782f6ad7e530cb212a59db32651fb16
Khushi-Pathak-25 <khushipathak.2503@gmail.com>
2025-12-26 15:31:06 +0530

    [Khushi] : Added program RemoveElement java file

---
+++ leetcode-codebase/array/RemoveElement.java
@@ -0,0 +1,15 @@
+// https://leetcode.com/problems/remove-element/submissions/1865698887
+// Problem Number - 27 
+
+class RemoveElement {
+    public int removeElement(int[] nums, int val) {
+        int index=0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]!=val){
+                nums[index]=nums[i];
+                index++;
+            }
+        }
+        return index;
+    }
+}
