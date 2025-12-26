cb63888f32f536ac32b980b88cc5541f3fecf03a
Khushi-Pathak-25 <khushipathak.2503@gmail.com>
2025-12-26 16:17:27 +0530

    [Khushi] : Added program RemoveDuplicatesFromSortedArray java file

--- leetcode-codebase/array/RemoveDuplicatesFromSortedArray.java
+++ leetcode-codebase/array/RemoveDuplicatesFromSortedArray.java
@@ -1,7 +1,10 @@
 // https://leetcode.com/problems/remove-duplicates-from-sorted-array/submissions/1865688363
 // Problem - 26
 
+import java.util.Scanner;
+
 class RemoveDuplicatesFromSortedArray {
+
     public int removeDuplicates(int[] nums) {
         int j = 0;
         for (int i = 1; i < nums.length; i++) {
@@ -11,5 +14,29 @@ class RemoveDuplicatesFromSortedArray {
             }
         }
         return j + 1;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        RemoveDuplicatesFromSortedArray obj = new RemoveDuplicatesFromSortedArray();
+
+        System.out.print("Enter array size: ");
+        int n = sc.nextInt();
+
+        int[] nums = new int[n];
+
+        System.out.println("Enter sorted array elements:");
+        for (int i = 0; i < n; i++) {
+            nums[i] = sc.nextInt();
+        }
+
+        int k = obj.removeDuplicates(nums);
+
+        System.out.println("Number of unique elements: " + k);
+        System.out.print("Array after removing duplicates: ");
+        for (int i = 0; i < k; i++) {
+            System.out.print(nums[i] + " ");
+        }
     }
 }
