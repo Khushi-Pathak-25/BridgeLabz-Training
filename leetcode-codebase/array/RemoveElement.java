88773e1b3fdfc116c511a7baa26766dff85231e0
Khushi-Pathak-25 <khushipathak.2503@gmail.com>
2025-12-26 16:14:41 +0530

    [Khushi] : Added program RemoveElement java file

--- leetcode-codebase/array/RemoveElement.java
+++ leetcode-codebase/array/RemoveElement.java
@@ -1,7 +1,9 @@
 // https://leetcode.com/problems/remove-element/submissions/1865698887
 // Problem Number - 27 
+import java.util.Scanner;
 
 class RemoveElement {
+	
     public int removeElement(int[] nums, int val) {
         int index=0;
         for(int i=0;i<nums.length;i++){
@@ -11,5 +13,32 @@ class RemoveElement {
             }
         }
         return index;
+    }
+	
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        RemoveElement obj = new RemoveElement();
+
+        System.out.print("Enter array size: ");
+        int n = sc.nextInt();
+
+        int[] nums = new int[n];
+
+        System.out.println("Enter array elements:");
+        for (int i = 0; i < n; i++) {
+            nums[i] = sc.nextInt();
+        }
+
+        System.out.print("Enter value to remove: ");
+        int val = sc.nextInt();
+
+        int k = obj.removeElement(nums, val);
+
+        System.out.println("Number of elements after removal: " + k);
+        System.out.print("Updated array: ");
+        for (int i = 0; i < k; i++) {
+            System.out.print(nums[i] + " ");
+        }
     }
 }
