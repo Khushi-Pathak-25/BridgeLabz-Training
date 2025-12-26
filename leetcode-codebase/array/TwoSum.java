1fc45444c3b16d2d4d8eeac0419f2a7dcdb83926
Khushi-Pathak-25 <khushipathak.2503@gmail.com>
2025-12-26 16:21:01 +0530

    [Khushi] : Added program TwoSum java file

--- leetcode-codebase/array/TwoSum.java
+++ leetcode-codebase/array/TwoSum.java
@@ -1,7 +1,10 @@
 // https://leetcode.com/problems/two-sum/submissions/1865669545
 // Problem number - 1
 
-class TwoSum {
+import java.util.Scanner;
+
+public class TwoSum {
+
     public int[] twoSum(int[] nums, int target) {
         for (int i = 0; i < nums.length - 1; i++) {
             for (int j = i + 1; j < nums.length; j++) {
@@ -10,5 +13,32 @@ class TwoSum {
             }
         }
         return new int[] {};
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        TwoSum obj = new TwoSum();
+
+        System.out.print("Enter array size: ");
+        int n = sc.nextInt();
+
+        int[] nums = new int[n];
+
+        System.out.println("Enter array elements:");
+        for (int i = 0; i < n; i++) {
+            nums[i] = sc.nextInt();
+        }
+
+        System.out.print("Enter target value: ");
+        int target = sc.nextInt();
+
+        int[] result = obj.twoSum(nums, target);
+
+        if (result.length == 2) {
+            System.out.println("Indices: " + result[0] + " " + result[1]);
+        } else {
+            System.out.println("No two sum solution found");
+        }
     }
 }
