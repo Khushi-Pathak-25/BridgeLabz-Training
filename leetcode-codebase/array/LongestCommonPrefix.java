5730469dfc048c8ff7127db60109fb07ef48e851
Khushi-Pathak-25 <khushipathak.2503@gmail.com>
2025-12-26 16:25:25 +0530

    [Khushi] : Added program LongestCommonPrefix java file

--- leetcode-codebase/array/LongestCommonPrefix.java
+++ leetcode-codebase/array/LongestCommonPrefix.java
@@ -2,13 +2,18 @@
 // https://leetcode.com/problems/longest-common-prefix/submissions/1865664304
 // Problem Number - 14
 
-class LongestCommonPrefix {
+import java.util.Scanner;
+
+public class LongestCommonPrefix {
+
     public String longestCommonPrefix(String[] strs) {
-        String result="";
-        if (strs == null || strs.length == 0){
+        String result = "";
+        if (strs == null || strs.length == 0) {
             return "";
-        } 
+        }
+
         String first = strs[0];
+
         for (int i = 0; i < first.length(); i++) {
             char ch = first.charAt(i);
 
@@ -18,8 +23,28 @@ class LongestCommonPrefix {
                 }
             }
             result += ch;
-
         }
         return result;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        LongestCommonPrefix obj = new LongestCommonPrefix();
+
+        System.out.print("Enter number of strings: ");
+        int n = sc.nextInt();
+        sc.nextLine(); // consume newline
+
+        String[] strs = new String[n];
+
+        System.out.println("Enter strings:");
+        for (int i = 0; i < n; i++) {
+            strs[i] = sc.nextLine();
+        }
+
+        String result = obj.longestCommonPrefix(strs);
+
+        System.out.println("Longest Common Prefix: " + result);
     }
 }
