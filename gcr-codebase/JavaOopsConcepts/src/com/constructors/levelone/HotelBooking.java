25adba9c4c70dcf41956472cf3ab2b3ccc578283
Khushi-Pathak-25 <khushipathak.2503@gmail.com>
2025-12-28 01:21:17 +0530

    [Khushi] : Added program HotelBooking java file

---
+++ gcr-codebase/JavaOopsConcepts/src/com/constructors/levelone/HotelBooking.java
@@ -0,0 +1,58 @@
+package com.constructors.levelone;
+
+import java.util.Scanner;
+
+public class HotelBooking {
+
+	// attributes - guestName, roomType, and nights
+	String guestName ;
+	String roomType ;
+	int nights ;
+	
+	// Default constructor
+	HotelBooking(){
+	    guestName = "XYZ";
+	    roomType = "Single";
+	    nights = 0;
+    }
+	
+	// Parameterized constructor
+	HotelBooking (String guestName, String roomType, int nights){
+		this.guestName = guestName;
+		this.roomType = roomType;
+		this.nights = nights;		
+	}
+	
+	// Copy constructor
+	HotelBooking(HotelBooking other){
+		this.guestName = other.guestName;
+		this.roomType = other.roomType;
+		this.nights = other.nights;
+	}
+	
+	// Display method
+	public void display() {
+		System.out.println("Guest Name -"+guestName +", "+"Room Type -"+roomType+", "+"Nights -"+nights);
+	}
+	
+	// main method
+	public static void main(String args[]) {
+		HotelBooking b1 = new HotelBooking() ;
+		b1.display();
+		
+		Scanner input = new Scanner(System.in);
+		System.out.println("Enter guest name : ");
+		String guestName = input.nextLine() ;
+		System.out.println("Enter room type : ");
+		String roomType = input.next();
+		System.out.println ("Enter stay details : ");
+		int nights = input.nextInt();
+		
+		HotelBooking b2 =new HotelBooking(guestName, roomType, nights );
+		b2.display();
+		
+		HotelBooking b3= new HotelBooking(b2);
+		b3.display();
+		
+	}
+}
