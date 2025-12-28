c6513078fca46e1b7b380eea492c0c649d22fd23
Khushi-Pathak-25 <khushipathak.2503@gmail.com>
2025-12-28 16:15:38 +0530

    [Khushi] : Added PostgraduateStudent java file

--- gcr-codebase/JavaOopsConcepts/src/com/constructors/levelone/PostgraduateStudent.java
+++ gcr-codebase/JavaOopsConcepts/src/com/constructors/levelone/PostgraduateStudent.java
@@ -12,15 +12,5 @@ public class PostgraduateStudent extends Student{
         System.out.println("Postgraduate Student Name: " + name);
     }
 
-    // main 
-    public static void main(String[] args) {
-        PostgraduateStudent pg = new PostgraduateStudent(111, "Ringo", 9.5);
-
-        pg.displayStudent();
-        pg.displayPostgraduate();
-
-        pg.setCGPA(9.0);
-        System.out.println("Updated CGPA: " + pg.getCGPA());
-    }
-	}
+}
 
