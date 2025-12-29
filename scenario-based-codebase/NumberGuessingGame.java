1772484d0a2085aff9eac88079b75d37769206b6
Khushi-Pathak-25 <khushipathak.2503@gmail.com>
2025-12-29 13:27:05 +0530

    [Khushi] : Added NumberGuessingGame java file

---
+++ scenario-based-codebase/NumberGuessingGame.java
@@ -0,0 +1,61 @@
+// The Number Guessing Game 🎲
+
+import java.util.Scanner;
+
+public class NumberGuessingGame{
+    public static void main(String args[]){
+	
+	    Scanner input = new Scanner(System.in);
+		
+		// Generate number between 1 and 100
+		int number = (int)(Math.random()*100)+1;
+		
+		int guess ;
+		int count = 0, totalAttempt = 5;
+		
+		System.out.println("Welcome to the Game!");
+        System.out.println("Guess a number between 1 and 100");
+        System.out.println("You have only 5 attempts.\n");
+		
+		// do-while loop
+		do{
+		    System.out.println("Enter a number");
+			guess = input.nextInt();
+			count ++;
+		    
+			if(number < guess){
+			    System.out.println("------------------------");
+			    System.out.println("Too high");
+				System.out.println("------------------------");
+			}
+			else if(number > guess) {
+			    System.out.println("------------------------");
+			    System.out.println("Too Low");
+				System.out.println("------------------------");
+			}
+			else{
+			    System.out.println("------------------------------------------------------------------------------");
+			    System.out.println("Congratulations ! You won ... You guessed the right number in attempts: "
+				+count);
+				System.out.println("------------------------------------------------------------------------------");
+				break;
+			}
+			
+			if (count < totalAttempt) {
+            System.out.println("Please! Try Again...");
+            }
+			
+		    if(count == totalAttempt){
+	        // exit statement
+		    System.out.println("Game Over ! Unable to guess the number... You used all 5 attempts");
+            System.out.println("The correct number was: " + number);
+		   }
+		}
+        
+        while(count < totalAttempt);		
+		
+        input.close();		
+	
+	}
+
+}
