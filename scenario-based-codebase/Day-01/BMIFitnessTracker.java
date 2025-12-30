6ba6d1f095b4f6ca6cfa72c0a692c5fd82949d31
Khushi-Pathak-25 <khushipathak.2503@gmail.com>
2025-12-30 12:19:15 +0530

    [Khushi] : added problem statement

--- scenario-based-codebase/Day-01/BMIFitnessTracker.java
+++ scenario-based-codebase/Day-01/BMIFitnessTracker.java
@@ -1,3 +1,10 @@
+/* Problem 2: Maya’s BMI Fitness Tracker
+Maya is a fitness coach who wants to calculate BMI for her clients.
+Write a Java program that takes height and weight as input,
+calculates BMI using the formula BMI = weight / (height * height), 
+and prints the BMI category as Underweight, Normal, or Overweight using if-else conditions. 
+Use meaningful variable names and proper comments to maintain clean code.*/
+
 import java.util.Scanner;
 
 public class BMIFitnessTracker {
