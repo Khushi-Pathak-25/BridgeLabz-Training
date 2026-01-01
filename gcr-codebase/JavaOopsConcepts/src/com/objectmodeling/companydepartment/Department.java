bba5f062714ab3f2d89289c1a0315033aa137fd7
Khushi-Pathak-25 <khushipathak.2503@gmail.com>
2026-01-01 16:35:53 +0530

    [Khushi] : added program companydepartment java files

---
+++ gcr-codebase/JavaOopsConcepts/src/com/objectmodeling/companydepartment/Department.java
@@ -0,0 +1,35 @@
+package com.objectmodeling.companydepartment;
+
+import java.util.ArrayList;
+
+public class Department {
+	
+	    // attributes
+		String departmentName;
+		ArrayList<Employee> employees;
+
+		// Constructor to initialize department
+		public Department(String departmentName) {
+			this.departmentName = departmentName;
+			employees = new ArrayList<>();
+		}
+
+		// Method to add employee to department
+		public void addEmployee(int id, String name) {
+			Employee employee = new Employee(id, name);
+			employees.add(employee);
+		}
+
+		// Method to display department details
+		public void displayDepartment() {
+			System.out.println("Department Name: " + departmentName);
+
+			// Loop to display all employees in department
+			for (Employee employee : employees) {
+				System.out.println("--------------------");
+				employee.displayEmployee();
+			}
+	    }
+}
+
+
