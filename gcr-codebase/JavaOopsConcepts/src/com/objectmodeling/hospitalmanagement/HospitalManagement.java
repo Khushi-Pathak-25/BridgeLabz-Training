4fb28b0518748de76779fa05431f0ef5ca3d3764
Khushi-Pathak-25 <khushipathak.2503@gmail.com>
2026-01-01 22:41:09 +0530

    [Khushi] : added question in file

--- gcr-codebase/JavaOopsConcepts/src/com/objectmodeling/hospitalmanagement/HospitalManagement.java
+++ gcr-codebase/JavaOopsConcepts/src/com/objectmodeling/hospitalmanagement/HospitalManagement.java
@@ -1,3 +1,14 @@
+/*Problem 3: Hospital, Doctors, and Patients (Association and Communication)
+Description: Model a Hospital where Doctor and Patient objects interact through consultations.
+ A doctor can see multiple patients, and each patient can consult multiple doctors.
+Tasks:
+Define a Hospital class containing Doctor and Patient classes.
+Create a method consult() in the Doctor class to show communication,
+ which would display the consultation between a doctor and a patient.
+Model an association between doctors and patients to show that doctors and patients can have multiple relationships.
+Goal: Practice creating an association with communication between objects by modeling doctor-patient consultations.
+*/
+
 package com.objectmodeling.hospitalmanagement;
 
 public class HospitalManagement {
