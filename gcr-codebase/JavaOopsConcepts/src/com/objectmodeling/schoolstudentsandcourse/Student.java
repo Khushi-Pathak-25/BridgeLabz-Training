1ff124188fac1e98b39e3ce8d4b07e6c97837517
Khushi-Pathak-25 <khushipathak.2503@gmail.com>
2026-01-01 17:11:44 +0530

    [Khushi] : added program schoolstudentsandcourse  java files

---
+++ gcr-codebase/JavaOopsConcepts/src/com/objectmodeling/schoolstudentsandcourse/Student.java
@@ -0,0 +1,32 @@
+package com.objectmodeling.schoolstudentsandcourse;
+
+import java.util.ArrayList;
+
+public class Student {
+        
+	    // attributes
+		String studentName;
+		ArrayList<Course> courses;
+
+		// Constructor to initialize student
+		public Student(String studentName) {
+			this.studentName = studentName;
+			courses = new ArrayList<>();
+		}
+
+		// Method to enroll student into a course (association)
+		public void enrollCourse(Course course) {
+			courses.add(course);
+			course.addStudent(this);
+		}
+
+		// Method to view enrolled courses
+		public void viewCourses() {
+			System.out.println("\nStudent Name: " + studentName);
+
+			// Loop to display enrolled courses
+			for (Course course : courses) {
+				System.out.println("Course: " + course.courseName);
+			}
+	    }
+}
