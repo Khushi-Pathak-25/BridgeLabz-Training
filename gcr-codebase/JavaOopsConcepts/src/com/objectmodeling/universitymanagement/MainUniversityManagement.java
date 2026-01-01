cb2f54fac6fc6dfecec513974d69341b8959d67a
Khushi-Pathak-25 <khushipathak.2503@gmail.com>
2026-01-01 22:39:04 +0530

    [Khushi] : added question in file

--- gcr-codebase/JavaOopsConcepts/src/com/objectmodeling/universitymanagement/MainUniversityManagement.java
+++ gcr-codebase/JavaOopsConcepts/src/com/objectmodeling/universitymanagement/MainUniversityManagement.java
@@ -1,3 +1,16 @@
+/*Problem 2: University with Faculties and Departments (Composition and Aggregation)
+Description: Create a University with multiple Faculty members and Department objects. 
+Model it so that the University and its Departments are in a composition relationship 
+(deleting a university deletes all departments),
+and the Faculty members are in an aggregation relationship 
+(faculty can exist outside of any specific department).
+Tasks:
+Define a University class with Department and Faculty classes.
+Demonstrate how deleting a University also deletes its Departments.
+Show that Faculty members can exist independently of a Department.
+Goal: Understand the differences between composition 
+and aggregation in modeling complex hierarchical relationships.*/
+
 package com.objectmodeling.universitymanagement;
 
 public class MainUniversityManagement {
