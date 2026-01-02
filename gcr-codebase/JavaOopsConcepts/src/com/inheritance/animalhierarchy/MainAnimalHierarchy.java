6f0369f9007a7e36642fa62ff634038e69680f60
Khushi-Pathak-25 <khushipathak.2503@gmail.com>
2026-01-02 11:41:57 +0530

    [Khushi] : added program animalhierarchy java files

--- gcr-codebase/JavaOopsConcepts/src/com/inheritance/animalhierarchy/MainAnimalHierarchy.java
+++ gcr-codebase/JavaOopsConcepts/src/com/inheritance/animalhierarchy/MainAnimalHierarchy.java
@@ -1,3 +1,12 @@
+/*Animal Hierarchy
+Description: Create a hierarchy where Animal is the superclass, 
+and Dog, Cat, and Bird are subclasses. Each subclass has a unique behavior.
+Tasks:
+Define a superclass Animal with attributes name and age, and a method makeSound().
+Define subclasses Dog, Cat, and Bird, each with a unique implementation of makeSound().
+Goal: Learn basic inheritance, method overriding, and polymorphism with simple classes.
+*/
+
 package com.inheritance.animalhierarchy;
 
 public class MainAnimalHierarchy {
