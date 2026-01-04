e3a4ad55b3a13277a7cbeeef1efd3e1f6ad577b5
Khushi-Pathak-25 <khushipathak.2503@gmail.com>
2026-01-04 16:57:13 +0530

    [Khushi] : Added program Library Management System

--- gcr-codebase/JavaOopsConcepts/src/com/encapsulationpolymorphism/librarymanagementsystem/MainLibrarayManagementSystem.java
+++ gcr-codebase/JavaOopsConcepts/src/com/encapsulationpolymorphism/librarymanagementsystem/MainLibrarayManagementSystem.java
@@ -1,3 +1,12 @@
+/*5. Library Management System
+Description: Develop a library management system:
+Use an abstract class LibraryItem with fields like itemId, title, and author.
+Add an abstract method getLoanDuration() and a concrete method getItemDetails().
+Create subclasses Book, Magazine, and DVD, overriding getLoanDuration() with specific logic.
+Implement an interface Reservable with methods reserveItem() and checkAvailability().
+Apply encapsulation to secure details like the borrower’s personal data.
+Use polymorphism to allow a general LibraryItem reference to manage all items, regardless of type.
+*/
 package com.encapsulationpolymorphism.librarymanagementsystem;
 
 import java.util.*;
