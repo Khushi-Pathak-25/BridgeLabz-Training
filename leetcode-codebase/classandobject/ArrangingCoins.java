73a58ac4953438211a63ee25ecde77838f337b78
Khushi-Pathak-25 <khushipathak.2503@gmail.com>
2026-01-06 00:39:53 +0530

    [Khushi] : Added program Arranging Coins java file

---
+++ leetcode-codebase/classandobject/ArrangingCoins.java
@@ -0,0 +1,32 @@
+// https://leetcode.com/problems/arranging-coins/submissions/1875696108
+// problem no- 441
+
+import java.util.Scanner;
+
+public class ArrangingCoins {
+
+    // Finds number of complete staircase rows
+    public int arrangeCoins(int n) {
+        int row = 0;
+
+        // Subtract coins row by row
+        while (n >= row + 1) {
+            row++;
+            n -= row;
+        }
+
+        return row;
+    }
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        LC0441_ArrangingCoins sol = new LC0441_ArrangingCoins();
+
+        System.out.print("Enter number of coins: ");
+        int n = input.nextInt();
+
+        System.out.println(sol.arrangeCoins(n));
+
+        input.close();
+    }
+}
