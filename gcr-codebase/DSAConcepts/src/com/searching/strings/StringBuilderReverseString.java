f96257a9f788b23fa60f7a4894964e327969c477
Khushi-Pathak-25 <khushipathak.2503@gmail.com>
2026-01-09 23:32:54 +0530

    [Khushi] : Added program Reverse a String Using StringBuilder

---
+++ gcr-codebase/DSAConcepts/src/com/searching/strings/StringBuilderReverseString.java
@@ -0,0 +1,31 @@
+
+package com.searching.strings;
+
+public class StringBuilderReverseString {
+
+	// Reverses a string using StringBuilder
+	public static String reverseString(String input) {
+
+		StringBuilder sb = new StringBuilder();
+
+		// Append string to StringBuilder
+		sb.append(input);
+
+		// Reverse the xstring
+		sb.reverse();
+
+		// Convert back to String
+		return sb.toString();
+	}
+
+	public static void main(String[] args) {
+
+		String input = "Hello World";
+
+		System.out.println("Original String: " + input);
+
+		String reversed = reverseString(input);
+
+		System.out.println("Reversed String: " + reversed);
+	}
+}
