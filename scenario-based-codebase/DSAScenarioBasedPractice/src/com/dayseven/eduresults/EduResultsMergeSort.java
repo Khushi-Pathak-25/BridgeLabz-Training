a051085d0e4d05f4997c7be81ea75642a763593e
Khushi-Pathak-25 <khushipathak.2503@gmail.com>
2026-01-19 22:46:06 +0530

    [Khushi] : Added java files for EduResults – Rank Sheet Generator scenario based problem

--- scenario-based-codebase/DSAScenarioBasedPractice/src/com/dayseven/eduresults/EduResultsMergeSort.java
+++ scenario-based-codebase/DSAScenarioBasedPractice/src/com/dayseven/eduresults/EduResultsMergeSort.java
@@ -1,26 +1,25 @@
 package com.dayseven.eduresults;
 
 public class EduResultsMergeSort {
-	
+
 	public static double[] merge(double marksBhopal[], double marksIndore[]) {
-		
+
 		int n1 = marksBhopal.length;
 		int n2 = marksIndore.length;
-		
-		double merged[]= new double[n1+n2];
-		
-		int i =0 ,j=0, k=0;
-		
-		while(i<n1 && j<n2) {
-			
-			if(marksBhopal[i]<marksIndore[j]) {
-			   merged[k++] =marksBhopal[i++];
-			}
-			else {
+
+		double merged[] = new double[n1 + n2];
+
+		int i = 0, j = 0, k = 0;
+
+		while (i < n1 && j < n2) {
+
+			if (marksBhopal[i] < marksIndore[j]) {
+				merged[k++] = marksBhopal[i++];
+			} else {
 				merged[k++] = marksIndore[j++];
 			}
 		}
-		
+
 		while (i < n1) {
 			merged[k++] = marksBhopal[i++];
 		}
@@ -29,15 +28,16 @@ public class EduResultsMergeSort {
 			merged[k++] = marksIndore[j++];
 		}
 
-		return merged;	
-		
+		return merged;
+
 	}
-	    // display 
-		public static void display(double merge[]) {
-			System.out.println("\n===== EduResults – Rank Sheet Generator =====");
-			for (double value : merge) {
-				System.out.print(value + " | ");
-			}
+
+	// display
+	public static void display(double merge[]) {
+		System.out.println("\n===== EduResults – Rank Sheet Generator =====");
+		for (double value : merge) {
+			System.out.print(value + " | ");
 		}
+	}
 
 }
