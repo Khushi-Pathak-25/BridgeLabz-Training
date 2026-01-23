9d4c3b0c8ad45d11b06307505367d07ab0037d60
Khushi-Pathak-25 <khushipathak.2503@gmail.com>
2026-01-23 12:05:12 +0530

    [Khushi] : Added program Generate Binary Numbers Using a Queue java file

---
+++ gcr-codebase/CollectionsConcepts/src/com/queueinterface/GenerateBinaryNumbers.java
@@ -0,0 +1,27 @@
+package com.queueinterface;
+import java.util.*;
+
+public class GenerateBinaryNumbers {
+
+
+	    public static List<String> generateBinary(int n) {
+	        List<String> result = new ArrayList<>();
+	        Queue<String> queue = new LinkedList<>();
+
+	        queue.add("1");
+
+	        for (int i = 0; i < n; i++) {
+	            String current = queue.poll();
+	            result.add(current);
+
+	            queue.add(current + "0");
+	            queue.add(current + "1");
+	        }
+	        return result;
+	    }
+
+	    public static void main(String[] args) {
+	        int n = 5;
+	        System.out.println(generateBinary(n));
+	    }
+	}
