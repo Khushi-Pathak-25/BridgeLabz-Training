23b3efecdc61274a23322e1b646de2a0545e3800
Khushi-Pathak-25 <khushipathak.2503@gmail.com>
2026-01-27 01:54:29 +0530

    [Khushi] : Added Extract Dates in dd/mm/yyyy Format Problem Java File.

---
+++ gcr-codebase/CollectionsConcepts/src/com/regex/extractdates/ExtractDates.java
@@ -0,0 +1,24 @@
+/**
+        * Word boundary (start)      - \b
+        * Two digit day              - \d{2}
+        * Date separator             - /
+        * Two digit month            - \d{2}
+        * Date separator             - /
+        * Four digit year            - \d{4}
+        * Word boundary (end)        - \b
+        */
+package com.regex.extractdates;
+
+import java.util.regex.*;
+
+public class ExtractDates {
+	public static void main(String[] args) {
+		String text = "The events are scheduled for 12/05/2023, 15/08/2024, and 29/02/2020.";
+
+		Matcher matcher = Pattern.compile("\\b\\d{2}/\\d{2}/\\d{4}\\b").matcher(text);
+
+		while (matcher.find()) {
+			System.out.print(matcher.group()+", ");
+		}
+	}
+}
