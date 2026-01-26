bd579f0d48dbf236c9a154eeebe1adb721a3ee4b
Khushi-Pathak-25 <khushipathak.2503@gmail.com>
2026-01-27 02:06:31 +0530

    [Khushi] : Added Censor Bad Words in a Sentence Problem Java File.

---
+++ gcr-codebase/CollectionsConcepts/src/com/regex/censorbadwords/CensorBadWords.java
@@ -0,0 +1,16 @@
+/**
+        * Word boundary (start)      - \b
+        * Alternation (OR) group     - (damn|stupid)
+        * Word boundary (end)        - \b
+        */
+package com.regex.censorbadwords;
+
+public class CensorBadWords {
+	public static void main(String[] args) {
+		String text = "This is a damn bad example with some stupid words.";
+
+		String regex = "\\b(damn|stupid)\\b";
+
+		System.out.println(text.replaceAll(regex, "****"));
+	}
+}
