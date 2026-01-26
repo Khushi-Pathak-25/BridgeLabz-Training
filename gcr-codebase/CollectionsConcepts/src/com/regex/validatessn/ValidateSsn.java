b333d4354f3b3682c0f7f988a1d274b300b6f736
Khushi-Pathak-25 <khushipathak.2503@gmail.com>
2026-01-27 02:25:43 +0530

    [Khushi] : Added Validate a Social Security Number (SSN) Problem Java File.

---
+++ gcr-codebase/CollectionsConcepts/src/com/regex/validatessn/ValidateSsn.java
@@ -0,0 +1,21 @@
+/**
+        * Start of string               - ^
+        * First three digits            - \d{3}
+        * Hyphen separator              - -
+        * Next two digits               - \d{2}
+        * Hyphen separator              - -
+        * Last four digits              - \d{4}
+        * End of string                 - $
+        */
+package com.regex.validatessn;
+
+public class ValidateSsn {
+	
+	public static void main(String[] args) {
+		String ssn = "123-45-6789";
+
+		String regex = "^\\d{3}-\\d{2}-\\d{4}$";
+
+		System.out.println(ssn.matches(regex) ? "Valid SSN" : "Invalid SSN");
+	}
+}
