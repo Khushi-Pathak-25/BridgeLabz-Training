1e06ac60789581c0124b2757f546581644ebef14
Khushi-Pathak-25 <khushipathak.2503@gmail.com>
2026-01-27 04:59:03 +0530

    [Khushi] : Added Program Create a @Todo Annotation for Pending Tasks Java Files.

---
+++ gcr-codebase/CollectionsConcepts/src/com/annotations/todo/ProjectTasks.java
@@ -0,0 +1,18 @@
+package com.annotations.todo;
+
+public class ProjectTasks {
+
+    // Pending feature
+    @Todo(task = "Implement login validation", assignedTo = "Ringo", priority = "HIGH")
+    public void loginFeature() {
+        
+	System.out.println("Login feature stub.");
+    }
+
+    // Another pending feature
+    @Todo(task = "Add export report option", assignedTo = "Mehul")
+    public void exportReport() {
+        
+	System.out.println("Export report stub.");
+    }
+}
