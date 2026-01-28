8a58e913ce50c0c39b5c816fe50913e422fdf15d
Khushi-Pathak-25 <khushipathak.2503@gmail.com>
2026-01-28 12:09:32 +0530

    [Khushi] :  Added Testing Date Formatter Problem Java Files.

---
+++ gcr-codebase/CollectionsConcepts/src/test/junit/dateformatter/DateFormatterTest.java
@@ -0,0 +1,21 @@
+package test.junit.dateformatter;
+
+import org.junit.jupiter.api.Test;
+
+import com.junit.dateformatter.DateFormatter;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class DateFormatterTest {
+    private final DateFormatter formatter = new DateFormatter();
+
+    @Test
+    void testValidDate() {
+        assertEquals("01-01-2023", formatter.formatDate("2023-01-01"));
+    }
+
+    @Test
+    void testInvalidDate() {
+        assertThrows(IllegalArgumentException.class, () -> formatter.formatDate("2023-13-01"));
+    }
+}
