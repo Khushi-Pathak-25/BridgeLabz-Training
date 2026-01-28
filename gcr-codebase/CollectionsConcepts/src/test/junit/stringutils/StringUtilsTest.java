56fee55bdb805f4532ed7b8efbed2218e895ee8b
Khushi-Pathak-25 <khushipathak.2503@gmail.com>
2026-01-28 13:17:02 +0530

    [Khushi] : Added Testing String Utility Methods Problem Java Files.

---
+++ gcr-codebase/CollectionsConcepts/src/test/junit/stringutils/StringUtilsTest.java
@@ -0,0 +1,27 @@
+package test.junit.stringutils;
+
+import org.junit.jupiter.api.Test;
+
+import com.junit.stringutils.StringUtils;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class StringUtilsTest {
+    private final StringUtils utils = new StringUtils();
+
+    @Test
+    void testReverse() {
+        assertEquals("olleh", utils.reverse("hello"));
+    }
+
+    @Test
+    void testIsPalindrome() {
+        assertTrue(utils.isPalindrome("radar"));
+        assertFalse(utils.isPalindrome("hello"));
+    }
+
+    @Test
+    void testToUpperCase() {
+        assertEquals("HELLO", utils.toUpperCase("hello"));
+    }
+}
