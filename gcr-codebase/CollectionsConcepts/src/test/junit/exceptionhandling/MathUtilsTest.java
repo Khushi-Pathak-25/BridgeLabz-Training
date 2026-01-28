66b618df42f68df66c8b2733a0298d6135958ea3
Khushi-Pathak-25 <khushipathak.2503@gmail.com>
2026-01-28 12:10:38 +0530

    [Khushi] :  Added Testing Exception Handling Problem Java Files.

---
+++ gcr-codebase/CollectionsConcepts/src/test/junit/exceptionhandling/MathUtilsTest.java
@@ -0,0 +1,17 @@
+package test.junit.exceptionhandling;
+
+import org.junit.jupiter.api.Test;
+
+import com.junit.exceptionhandling.MathUtils;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class MathUtilsTest {
+	
+    private final MathUtils utils = new MathUtils();
+
+    @Test
+    void testDivideThrowsException() {
+        assertThrows(ArithmeticException.class, () -> utils.divide(10, 0));
+    }
+}
